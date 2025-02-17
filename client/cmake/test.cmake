2ab213dab0bf0d0ed39620725ff4543493bbd5d0
ksalwin <kasjan.salwin@gmail.com>
2025-02-17 22:01:38 +0100

    CHANGE CMake script to use modular sources for test build

--- client/cmake/test.cmake
+++ client/cmake/test.cmake
@@ -4,11 +4,6 @@ enable_testing()
 set(GTEST_DIR ../dependencies/third_party/googletest)
 add_subdirectory(${GTEST_DIR} ${CMAKE_BINARY_DIR}/googletest)
 
-set(TEST_SOURCES
-	test/main.cpp)
-
-add_executable(test-${PROJECT_NAME} ${TEST_SOURCES})
-
 target_link_libraries(test-${PROJECT_NAME} PRIVATE gtest gtest_main)
 
 add_test(NAME test-${PROJECT_NAME} COMMAND test-${PROJECT_NAME})
