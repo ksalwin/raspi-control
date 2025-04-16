9f8fc986194e9bb52392e01af3dda9a0d5ad4080
ksalwin <>
2025-04-16 21:08:03 +0200

    Improved print usage formatting

--- run.sh
+++ run.sh
@@ -43,13 +43,17 @@ function ensure_tool_installed() {
 
 function print_usage() {
 	echo "[INFO] Usage:"
-	echo "sh $0 <client|server> <command>"
+	echo "  sh $0 <client|server> <command>"
 	echo
-	echo "Possible commands:"
-	echo "\tbuild - build client"
-	echo "\tclean - remove build artifacts"
-	echo "\trun   - build and run client app"
-	echo "\ttest  - test client"
+	echo "Available commands:"
+	echo "  build    - Configure and build the project"
+	echo "  clean    - Remove build artifacts"
+	echo "  run      - Build and run the executable"
+	echo "  test     - Build with tests and run them"
+	echo "  doc      - Generate documentation (requires 'doc' and 'plantuml')"
+	echo
+	echo "Example:"
+	echo "  sh $0 client build"
 }
 
 function make_build_dir() {
