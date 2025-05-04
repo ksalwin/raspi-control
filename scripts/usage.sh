c4d98e422b9a9973b050977fc72a645d5e48dce0
ksalwin <>
2025-05-04 14:13:40 +0200

    Add separate target usage message

--- scripts/usage.sh
+++ scripts/usage.sh
@@ -1,14 +1,20 @@
 function print_usage() {
-	echo "[INFO] Usage:"
+	echo "Usage:"
 	echo "  sh $0 <client|server> <command>"
 	echo
-	echo "Available commands:"
+	echo "Available 'client' commands:"
 	echo "  build    - Configure and build the project"
 	echo "  clean    - Remove build artifacts"
 	echo "  run      - Build and run the executable"
 	echo "  test     - Build with tests and run them"
 	echo "  doc      - Generate documentation (requires 'doc' and 'plantuml')"
 	echo
+	echo "Available 'server' commands:"
+	echo "  build  <raspi_user> <raspi_ip>  - Configure and build the project"
+	echo "  clean    - Remove build artifacts"
+	echo "  deploy   - Copy binaries to Raspberry Pi"
+	echo
 	echo "Example:"
 	echo "  sh $0 client build"
+	echo
 }
